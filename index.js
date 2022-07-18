6b7a6bd0f2a7b9fc929b40fe696c6cb6d6a4ea46
Parker Parham <parkerparham@gmail.com>
2022-07-18 11:40:44 -0400

    commit tree 3fc3149b959158dd3ce3f8bf914348920198d283

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Jones';
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'; //concantenation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; //interpolation
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
